8517b104684d6619ec0eed00c54e8702c309cde7
crispaulrubi <crispaul@mytdevcorp.com>
2021-03-11 00:02:54 +0800

    initial commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/UP-HIGH-SET';
+	public $baseURL = 'http://localhost/UP-HIGH-SET';
 
 	/**
 	 * --------------------------------------------------------------------------

0a766b13ae223825cac60f1da3a0156dddc6a9c2
Rosalie Razonable <>
2021-03-18 17:36:05 +0800

    adding and implementing cryptographically secure random password

--- app/Helpers/Autopass_helper.php
+++ app/Helpers/Autopass_helper.php
@@ -2,5 +2,18 @@
 
 function randomize_password()
 {
+    try {
+      $password = random_bytes(8);
+  } catch (TypeError $e) {
+      // Well, it's an integer, so this IS unexpected.
+      die("An unexpected error has occurred");
+  } catch (Error $e) {
+      // This is also unexpected because 32 is a reasonable integer.
+      die("An unexpected error has occurred");
+  } catch (Exception $e) {
+      // If you get this message, the CSPRNG failed hard.
+      die("Could not generate a random string. Is our OS secure?");
+  }
 
+  return bin2hex($password);
 }

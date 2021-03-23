7250b3691edc35085921f226313cd8cef039042b
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-03-23 13:44:06 +0800

    indentation fix

--- app/Views/pageTemplate.php
+++ app/Views/pageTemplate.php
@@ -27,43 +27,42 @@
     <!-- header section ends -->
     <div class="wrapper d-flex align-items-stretch">    
     <!-- Sidebar Holder -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <div class="img bg-wrap text-center py-4" style="background-image: url(<?=base_url()?>public/samplecover.jpg);">
-                <div class="user-logo">
-                    <img class="rounded-cricle" src="<?=base_url()?>/public/LogoCitronella.png">
-                    <h3>CITRONELLA</h3>
-                </div>
-            </div>
-        </div>
+      <nav id="sidebar">
+          <div class="sidebar-header">
+              <div class="img bg-wrap text-center py-4" style="background-image: url(<?=base_url()?>public/samplecover.jpg);">
+                  <div class="user-logo">
+                      <img class="rounded-cricle" src="<?=base_url()?>/public/LogoCitronella.png">
+                      <h3>CITRONELLA</h3>
+                  </div>
+              </div>
+          </div>
 
-        <ul class="list-unstyled components">
-            <li>
-                <a href="#"><i class="bi bi-person-circle"></i>  Profile</a>
-                <a href="#"><i class="bi bi-gear-wide-connected"></i>  Settings and Privacy</a>
-                <a href="#pageSubmenu"><i class="bi bi-house-fill"></i> Dashboard</a>
-                <a href="#"><i class="bi bi-zoom-in"></i> About</a>
-                <a href="#"><i class="bi bi-telephone-fill"></i> Contact</a>
-            </li>
-        </ul>
-        <ul class="list-unstyled CTAs">
-            <a class="nav-link" href="#"><input type="button" value=" View Report" name="viewResults"></a>
-            <a class="nav-link" href="#"><input type="button" value="Print Report" name="printReports"></a>
-            <div class="logout">
-                <a class="nav-link" id="logout" href="#"><input type="button" value="LOGOUT" name="logOut"></a>
-            </div>
-        </ul>
-    </nav>
-    <!-- Page Content Holder -->
-    <div class="navIcon">
-        <div class="navbar-header">
-            <button type="button" id="sidebarCollapse" class="navbar-btn"89>
-            <i class="bi bi-list fa-2x"></i>
-                
-            </button>
-        </div>
-    </div>
-    <?= $this->renderSection('content');?>
+          <ul class="list-unstyled components">
+              <li>
+                  <a href="#"><i class="bi bi-person-circle"></i>  Profile</a>
+                  <a href="#"><i class="bi bi-gear-wide-connected"></i>  Settings and Privacy</a>
+                  <a href="#pageSubmenu"><i class="bi bi-house-fill"></i> Dashboard</a>
+                  <a href="#"><i class="bi bi-zoom-in"></i> About</a>
+                  <a href="#"><i class="bi bi-telephone-fill"></i> Contact</a>
+              </li>
+          </ul>
+          <ul class="list-unstyled CTAs">
+              <a class="nav-link" href="#"><input type="button" value=" View Report" name="viewResults"></a>
+              <a class="nav-link" href="#"><input type="button" value="Print Report" name="printReports"></a>
+              <div class="logout">
+                  <a class="nav-link" id="logout" href="#"><input type="button" value="LOGOUT" name="logOut"></a>
+              </div>
+          </ul>
+      </nav>
+      <!-- Page Content Holder -->
+      <div class="navIcon">
+          <div class="navbar-header">
+              <button type="button" id="sidebarCollapse" class="navbar-btn"89>
+                <i class="bi bi-list fa-2x"></i>                  
+              </button>
+          </div>
+      </div>
+      <?= $this->renderSection('content');?>
     </div>
     <!-- footer section starts  -->
 

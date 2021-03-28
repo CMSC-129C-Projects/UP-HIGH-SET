0e038747d8ffe70e7ae4d435feede6543148ed8a
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-03-29 00:28:00 +0800

    added routes for Update::index parameters

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,8 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->add('update/admin', 'Update::index/admin');
+$routes->add('update/student', 'Update::index/student');
 
 /*
  * --------------------------------------------------------------------

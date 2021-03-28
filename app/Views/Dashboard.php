afb1e1c10fb931ab48c4b7b1bd57547f081cd169
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-03-28 22:27:35 +0800

    dropdown button on view users

--- app/Views/Dashboard.php
+++ app/Views/Dashboard.php
@@ -15,7 +15,13 @@
                 <nav class="nav">
                     <a class="nav-link active" href="<?=base_url();?>/update/add"><input type="button" value="Add User" name="addUser"></a>
                     <a class="nav-link" href="#"><input type="button" value="Evaluation Status" name="setEvalPeriod"></a>
-                    <a class="nav-link" href="<?=base_url();?>/update"><input type="button" value="View Users" name="removeUser  "></a>
+                    <div class="dropdown">
+                        <a href="#" class="nav-link dropbtn" role="button"><input type="button" value="View Users" name="removeUser"></a>
+                        <div class="dropdown-content">
+                            <a class="dropdown-item" href="<?=base_url();?>/update"><input type="button" value="View Students"></a>
+                            <a class="dropdown-item" href="#"><input type="button" value="View Admin"></a>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </section>

670386561f6cd4f9466409c1cbb640a57bc2477a
rosalierazonable <rsrazonable1@up.edu.ph>
2021-03-31 23:31:46 +0800

    register created filters

--- app/Filters/NoAuth.php
+++ app/Filters/NoAuth.php
@@ -5,7 +5,7 @@ use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class NoAuth implements FilterInterface
+class Noauth implements FilterInterface
 {
   public function before(RequestInterface $request, $arguments = null)
    {

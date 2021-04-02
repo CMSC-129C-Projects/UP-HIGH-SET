f5021b6db014f55dab61951277ed5c739382cd16
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-04-02 15:20:50 +0800

    remove role buttons if specific add role is clicked

--- app/Helpers/Form_helper.php
+++ app/Helpers/Form_helper.php
@@ -19,17 +19,11 @@ function setFormBasedOnRole($role) {
                 </li>';
     if(isset($role) && $role === 'student') {
         return '<li class="nav-item">
-                    <a href="#Admin" class="nav-link" data-toggle="tab" id="btn-admin"><input type="button" value="Admin" disabled></a>
-                </li>
-                <li class="nav-item">
                     <a href="#Student" class="nav-link active" data-toggle="tab" id="btn-student"><input type="button" value="Student"></a>
                 </li>';
     } elseif(isset($role) && $role === 'admin') {
         return '<li class="nav-item">
                     <a href="#Admin" class="nav-link" data-toggle="tab" id="btn-admin"><input type="button" value="Admin" disabled></a>
-                </li>
-                <li class="nav-item">
-                    <a href="#Student" class="nav-link active" data-toggle="tab" id="btn-student"><input type="button" value="Student" disabled></a>
                 </li>';
     } else {
         return $default;

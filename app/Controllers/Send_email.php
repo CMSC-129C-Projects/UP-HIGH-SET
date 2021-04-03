e0151398328d6f5a6edce707dd6f5967a64dc7d8
rosalierazonable <rsrazonable1@up.edu.ph>
2021-04-03 17:42:22 +0800

    Implement dynamic email content with database

--- app/Controllers/Send_email.php
+++ app/Controllers/Send_email.php
@@ -1,19 +1,25 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\EmailModel;
+
 class Send_email extends BaseController {
 
   public function index() {
     $data = [];
     $rules = [
       'email_subject' => 'required',
-      'email_content' => 'required'
+      'email_content' => 'required',
+      'purpose' => 'required'
     ];
 
     $data['validation'] = null;
 
     $email_subject = $this->request->getVar('email_subject');
     $email_content = $this->request->getVar('email_content');
+    $email_purpose = $this->request->getVar('purpose');
+
+    // echo "$email_purpose" ;
 
     $css = ['custom/styles.css'];
 
@@ -22,6 +28,15 @@ class Send_email extends BaseController {
       if($this->validate($rules))
       {
         $data['css'] = addExternal($css, 'css');
+        $emailModel = new EmailModel();
+
+        $q_data = [
+          'title' => $email_subject,
+          'message' => $email_content,
+          'purpose' => $email_purpose
+        ];
+
+        $emailModel->insert($q_data);
       } else {
         $data = [
           'css'   => addExternal($css, 'css'),
--- app/Views/email/send_email.php
+++ app/Views/email/send_email.php
@@ -1,4 +1,4 @@
-<?= $this->extend('pageTemplate');?>
+<?= $this->extend('template/pageTemplate');?>
 
 
 <?= $this->section('content');?>
@@ -15,12 +15,27 @@
           <div class="form-group">
             <label for="email-subject" style="margin-top: 1rem;"> Email Subject</label>
             <input type="text" class="form-control" name = 'email_subject' id="email-subject" value="<?=set_value('email_subject')?>" placeholder="Enter Email subject here ...">
+            <br>
             <span class="text-danger"><?=displaySingleError($validation, 'email_subject');?></span>
           </div>
           <div class="form-group">
             <label for="email-content">Email Content</label>
             <textarea class="form-control" name="email_content" id="email-content" value="<?=set_value('email_content')?>" rows="3" placeholder="Email notification content ..."></textarea>
+            <br>
             <span class="text-danger"><?=displaySingleError($validation, 'email_content');?></span>
+          </div>
+          <div class="form-group">
+            <label for="purpose">Purpose</label>
+            <select name="purpose" id="purpose">
+              <option value="">Select...</option>
+              <option value="announcement">Announcement</option>
+              <option value="registration">Registration</option>
+              <option value="evaluation">Evaluation</option>
+              <option value="change_pass">Change Password</option>
+              <option value="forgot_pass">Forgot Password</option>
+            </select>
+            <br>
+            <span class="text-danger"><?=displaySingleError($validation, 'purpose');?></span>
           </div>
 
           <p>Attachment is Optional</p>

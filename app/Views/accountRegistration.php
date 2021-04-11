cd161c7316e95e0e57fe289b4772a56e7c6a3e64
rosalierazonable <rsrazonable1@up.edu.ph>
2021-04-12 04:39:36 +0800

    refactor and reformat account registration page

--- app/Views/accountRegistration.php
+++ app/Views/accountRegistration.php
@@ -18,168 +18,139 @@
     </div>
   <?php endif;?>
   <section id="register" class="container-fluid">
-    <div class="heading text-center">
+    <div class="heading text-center" style="margin-bottom: 1.5rem;">
       <h1>User Registration</h1>
     <div>
 
-    <div class="row-md-6">
-      <ul class="nav nav-tabs">
-        <?=setFormBasedOnRole($role);?>
-      </ul>
-    </div>
-    <div class="tab-content">
-      <?php if(isset($role) && $role === 'student'):?>
-        <div class="tab-pane fade" id="Admin">
-      <?php else:?>
-        <div class="tab-pane fade active show" id="Admin">
-      <?php endif;?>
-        <div class="row justify-content-center ">
-          <form action="<?=base_url()?>/update/add/admin" method="post" class="add">
-            <div class="row">
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox fname">
-                    <input type="text" name="adminFirstName" value="<?=set_value('adminFirstName');?>" required>
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'adminFirstName');?></span>
-                    <h3>First Name</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox lname">
-                    <input type="text" name="adminLastName" value="<?=set_value('adminLastName');?>" required>
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'adminLastName');?></span>
-                    <h3>Last Name</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox uname">
-                    <input type="text" name="adminUserName" value="<?=set_value('adminUserName');?>">
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'adminUserName');?></span>
-                    <h3>User Name</h3>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox contactnum">
-                    <input type="text" name="adminContactNum" value="<?=set_value('adminContactNum');?>" required>
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'adminContactNum');?></span>
-                    <h3>Contact Number</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row-md-4 email">
-              <div class="mailBox">
-                <div class="inputBox">
-                  <input type="email" name="adminEmail" value="<?=set_value('adminEmail');?>" required>
-                  <br>
-                  <span class="text-danger"><?=displaySingleError($validation, 'adminEmail');?></span>
-                  <h3>Email</h3>
-                </div>
-              </div>
-            </div>
-            <input type="submit" value="submit">
-            <button onclick="window.location.href='<?=base_url();?>/update/admin';" id="cancel" type="button" name="cancel">Cancel</button>
-          </form>
+    <div class="tabs">
+      <div class="tab-content" style="border-top-left-radius: 0 !important; border-top-right-radius: 0 !important;">
+        <div class="row-md-6">
+          <ul class="nav nav-tabs">
+            <?=setFormBasedOnRole($role);?>
+          </ul>
         </div>
-      </div>
-      <?php if(isset($role) && $role === 'student'):?>
-        <div class="tab-pane fade active show" id="Student">
-      <?php else:?>
-        <div class="tab-pane fade" id="Student">
-      <?php endif;?>
-        <div class="row justify-content-center">
-          <form action="<?=base_url()?>/update/add/student" method="post" class="add">
-            <div class="row">
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox">
-                      <input type="text" name="studFirstName" value="<?=set_value('studFirstName');?>" required>
+        <?php if(isset($role) && $role === 'student'):?>
+          <div class="tab-pane fade" id="Admin">
+        <?php else:?>
+          <div class="tab-pane fade active show" id="Admin">
+        <?php endif;?>
+          <div class="row justify-content-center ">
+            <form action="<?=base_url()?>/update/add/admin" method="post" class="add">
+              <div class="row">
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox fname">
+                      <input type="text" name="adminFirstName" value="<?=set_value('adminFirstName');?>" required>
                       <br>
-                      <span class="text-danger"><?=displaySingleError($validation, 'studFirstName');?></span>
+                      <span class="text-danger"><?=displaySingleError($validation, 'adminFirstName');?></span>
                       <h3>First Name</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox">
-                      <input type="text" name= "studLastName" value="<?=set_value('studLastName');?>" required>
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox lname">
+                      <input type="text" name="adminLastName" value="<?=set_value('adminLastName');?>" required>
                       <br>
-                      <span class="text-danger"><?=displaySingleError($validation, 'studLastName');?></span>
+                      <span class="text-danger"><?=displaySingleError($validation, 'adminLastName');?></span>
                       <h3>Last Name</h3>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox">
-                      <input type="text" name="studNum" value="<?=set_value('studNum');?>" required>
+              <div class="row">
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox uname">
+                      <input type="email" name="adminEmail" value="<?=set_value('adminEmail');?>" required>
                       <br>
-                      <span class="text-danger"><?=displaySingleError($validation, 'studNum');?></span>
-                      <h3>Student Number</h3>
+                      <span class="text-danger"><?=displaySingleError($validation, 'adminEmail');?></span>
+                      <h3>Email</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox">
-                      <input type="text" name="studUserName" value="<?=set_value('studUserName');?>">
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox contactnum">
+                      <input type="text" name="adminContactNum" value="<?=set_value('adminContactNum');?>" required>
                       <br>
-                      <span class="text-danger"><?=displaySingleError($validation, 'studUserName');?></span>
-                      <h3>User Name</h3>
+                      <span class="text-danger"><?=displaySingleError($validation, 'adminContactNum');?></span>
+                      <h3>Contact Number</h3>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6 half">
-                <div class="form-group">
+              <input type="submit" value="submit">
+              <button onclick="window.location.href='<?=base_url();?>/update/admin';" id="cancel" type="button" name="cancel">Cancel</button>
+            </form>
+          </div>
+        </div>
+        <?php if(isset($role) && $role === 'student'):?>
+          <div class="tab-pane fade active show" id="Student">
+        <?php else:?>
+          <div class="tab-pane fade" id="Student">
+        <?php endif;?>
+          <div class="row justify-content-center">
+            <form action="<?=base_url()?>/update/add/student" method="post" class="add">
+              <div class="row">
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox">
+                        <input type="text" name="studFirstName" value="<?=set_value('studFirstName');?>" required>
+                        <br>
+                        <span class="text-danger"><?=displaySingleError($validation, 'studFirstName');?></span>
+                        <h3>First Name</h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox">
+                        <input type="text" name= "studLastName" value="<?=set_value('studLastName');?>" required>
+                        <br>
+                        <span class="text-danger"><?=displaySingleError($validation, 'studLastName');?></span>
+                        <h3>Last Name</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox">
+                        <input type="text" name="studNum" value="<?=set_value('studNum');?>" required>
+                        <br>
+                        <span class="text-danger"><?=displaySingleError($validation, 'studNum');?></span>
+                        <h3>Student Number</h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 half">
+                  <div class="form-group">
+                    <div class="inputBox">
+                      <input type="text" name="gradeLevel" value="<?=set_value('gradeLevel');?>" required>
+                      <br>
+                      <span class="text-danger"><?=displaySingleError($validation, 'gradeLevel');?></span>
+                      <h3>Grade Level</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row-md-4">
+                <div class="mailBox">
                   <div class="inputBox">
-                    <input type="text" name="gradeLevel" value="<?=set_value('gradeLevel');?>" required>
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'gradeLevel');?></span>
-                    <h3>Grade Level</h3>
+                      <input type="email" name="studEmail" value="<?=set_value('studEmail');?>" required>
+                      <br>
+                      <span class="text-danger"><?=displaySingleError($validation, 'studEmail');?></span>
+                      <h3>Email</h3>
                   </div>
                 </div>
               </div>
-              <div class="col-md-6 half">
-                <div class="form-group">
-                  <div class="inputBox">
-                    <input type="text" name="studContactNum" value="<?=set_value('studContactNum');?>">
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'studContactNum');?></span>
-                    <h3>Contact Number</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row-md-4">
-              <div class="mailBox">
-                <div class="inputBox">
-                    <input type="email" name="studEmail" value="<?=set_value('studEmail');?>" required>
-                    <br>
-                    <span class="text-danger"><?=displaySingleError($validation, 'studEmail');?></span>
-                    <h3>Email</h3>
-                </div>
-              </div>
-            </div>
-            <input type="submit" value="submit">
-            <button onclick="window.location.href='<?=base_url();?>/update/student';" id="cancel" type="button" name="cancel">Cancel</button>
-          </form>
+              <input type="submit" value="submit">
+              <button onclick="window.location.href='<?=base_url();?>/update/student';" id="cancel" type="button" name="cancel">Cancel</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>

5a44fafdf1e6ca14a18f8665af9e00a3a9f5e088
Christian Dagondon <cpdagondon@up.edu.ph>
2021-04-29 01:51:26 +0800

    Some updates

--- app/Views/account_updates/profileUpdate.php
+++ app/Views/account_updates/profileUpdate.php
@@ -5,14 +5,15 @@
 
 <?= $this->section('content');?>
 <head>
-  
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="<?=base_url()?>/public/css/custom/profileUpdate/pUpdate.css" rel="stylesheet">
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  
-  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
 </head>
 
@@ -57,7 +58,7 @@
             
 
               
-          <div class="content">
+          <div class="tab-content">
             
                 <hr>
                   <form class="form" action="##" method="post" id="registrationForm">

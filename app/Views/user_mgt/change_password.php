501ff64f63696f690800b74e4006b7f2a19feebb
Patrick Castro <jgcastro6@up.edu.ph>
2021-05-04 17:40:02 +0800

    fix responsiveness

--- app/Views/user_mgt/change_password.php
+++ app/Views/user_mgt/change_password.php
@@ -1,12 +1,12 @@
 <?php
   if($is_changed) {
     $this->extend('template/pageTemplate');
-    $url = base_url('profile');
+    $url = base_url('dashboard');
   } else {
     $this->extend('template/default');
     $url = base_url('login');
   }
-?>
+?>  
 
 <?php $this->section('content'); ?>
 
@@ -15,20 +15,19 @@
         <h1 style= "padding:4.7rem;">Change Password</h1>
       </div>
       <div id="ChangePassword">
-        <div class="card" style="width:70%; margin:auto; margin-bottom: 10rem">
+        <div class="card" style="width:70%; margin:auto;">
           <div class="card-body" style="padding: 40px; margin: 10px">
             <?php if(isset($error)) { ?>
             <h3 class="card-title" style="color: #7b1113; margin-bottom: 20px; font-size: 20px;">An error occurred.</h3>
             <div class="alert alert-danger" style="padding:20px;">
               <h4><?=$error?><h4>
-                <?php $url = base_url('change_password'); ?>
             </div>
             <br>
             <a class="button" style="border-radius: 2rem !important; padding: 12px 21px; font-size: 13px; margin-bottom:20px; " href="<?=$url?>">Back</a>
             <?php } else { ?>
             <h3 class="card-title" style="color: #7b1113; margin-bottom: 20px; font-size: 20px;">Fill up the form to change password.</h3>
             <br>
-            <form class="reset_pass" action="<?=base_url('change_password')?>" method="post">
+            <form class="reset_pass" action="<?=base_url('home/change_password')?>" method="post">
               <div class="form-group">
                 <label for="old_pass" style="margin-top: 1rem; font-size:15px; margin-bottom:7px">Old Password:</label>
                 <input id="old_pass" class="form-control" type="password" name="old_pass" value="" placeholder="Enter Old Password..." required>

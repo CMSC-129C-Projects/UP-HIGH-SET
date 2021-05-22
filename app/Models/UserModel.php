6b716b33f5eec75a509a2653bb38937811b97424
rosalierazonable <rsrazonable1@up.edu.ph>
2021-05-22 15:52:15 +0800

    add helper function for getting total number of students per subject

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -31,4 +31,24 @@ class UserModel extends Model {
     protected $updatedField  = 'updated_on';
 
     protected $skipValidation = true;
+
+    public function get_all_students_per_subject($id) {
+      $db = \Config\Database::connect();
+
+      $sql = <<<EOT
+SELECT count(users.id) as student_perSub
+FROM users
+LEFT JOIN subjects
+ON users.grade_level = subjects.grade_level
+WHERE
+users.is_deleted = 0 AND users.is_active = 1 AND
+users.role = 2 AND subjects.is_deleted = 0 AND
+subjects.id = $id
+EOT;
+
+      $query = $db->query($sql);
+      $result = $query->getResult();
+
+      return $result[0]->student_perSub;
+  }
 }

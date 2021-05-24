bbe72592402d38a8cdce79e484004d29c4826a99
Christian Dagondon <cpdagondon@up.edu.ph>
2021-05-24 22:46:25 +0800

    Ari makita ang mga subjects nga gi handle sa mga profs

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -13,6 +13,6 @@ class Test extends BaseController
         //     'css' => addExternal($css, 'css'),
         //     'js'  => addExternal($js, 'javascript')
         // ];
-        return view('Evaluation/evaluationSubjects');
+        return view('professors/viewsubjects');
     }
 }
---
+++ app/Views/professors/viewsubjects.php
@@ -0,0 +1,46 @@
+<?= $this->extend('template/pageTemplate');?>
+
+<?= $this->section('content');?>
+<head>
+<link href="<?=base_url()?>/public/css/custom/profs/viewsubjects-style.css" rel="stylesheet">
+</head>
+
+<section id="special" class="container-fluid">
+
+<div class="heading text-center">
+<h1>PROFESSOR ROBERTO BASADRE</h1>
+<h3> Civil Engineer, UPHS Faculty</h3>
+
+
+</div>
+
+
+<div class="card-container">
+
+    <div class="card">
+        <img src="<?=base_url();?>/public/images/SubjectCover.jpg" alt="">
+        <h1> PRE-CALCULUS</h1>
+        <p> 42 STUDENTS ENROLLED.</p>
+        <a href="#"><button>View Progress</button></a>
+    </div>
+
+    <div class="card">
+        <img src="<?=base_url();?>/public/images/SubjectCover.jpg" alt="">
+        <h1> GENERAL MATH</h1>
+        <p>42 STUDENTS ENROLLED.</p>
+        <a href="#"><button>View Progress</button></a>
+    </div>
+
+    <div class="card">
+        <img src="<?=base_url();?>/public/images/SubjectCover.jpg" alt="">
+        <h1>CALCULUS </h1>
+        <p>42 STUDENTS ENROLLED.</p>
+        <a href="#"><button>View Progress</button></a>
+    </div>
+
+</div>
+
+</section>
+
+
+<?= $this->endSection();?>

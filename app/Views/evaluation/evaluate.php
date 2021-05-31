614d4f33e95886d9ed8f6517c89474a7e9c24c45
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-05-31 20:41:47 +0800

    review done

--- app/Views/evaluation/evaluate.php
+++ app/Views/evaluation/evaluate.php
@@ -14,197 +14,69 @@
           <div class="modal-body" style="padding: 25px;">
             <i style="font-size: 15px;"> Please double-check your answers before submitting this form. Your review will be unmodifiable after submission. </i>
             <br><br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;">Instructional Skills</p>
-            </div>
-            <div class="row" style="margin-top: 3px;">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> Has mastery of the subject matter. </p>
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-12" style="text-align:center;">
-                    <li>
-                      <i style="font-size: 13px;"> Excellent </i>
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Excellent">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Very Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Fair">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Poor">
-                    </li>
-                    <li>
-                      <i style="font-size: 13px;"> Poor </i>
-                    </li>
+            <?php $index = 0;?>
+            <?php foreach($questions as $key => $value):?>
+                <div class="row" style="background-color:#7b1113;">
+                    <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;"><?=$key?></p>
                 </div>
-            </div>
-            <br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;"> Class Management</p>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> Corrects and gives results and feedback of examinations and/or other work within reasonable time. </p>
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-12" style="text-align:center;">
-                    <li>
-                      <i style="font-size: 13px;"> Excellent </i>
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Excellent">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Very Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Fair">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Poor">
-                    </li>
-                    <li>
-                      <i style="font-size: 13px;"> Poor </i>
-                    </li>
-                </div>
-            </div>
-            <br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;">Personal Qualities</p>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> Has high intellectual standard. </p>
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-12" style="text-align:center;">
-                    <li>
-                      <i style="font-size: 13px;"> Excellent </i>
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Excellent">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Very Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Fair">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Poor">
-                    </li>
-                    <li>
-                      <i style="font-size: 13px;"> Poor </i>
-                    </li>
-                </div>
-            </div>
-            <br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;"> Student-Faculty Relations </p>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> Maintains cordial but professional relations with students. </p>
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-12" style="text-align:center;">
-                    <li>
-                      <i style="font-size: 13px;"> Excellent </i>
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Excellent">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Very Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Fair">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Poor">
-                    </li>
-                    <li>
-                      <i style="font-size: 13px;"> Poor </i>
-                    </li>
-                </div>
-            </div>
-            <br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;">General Evaluation</p>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> Taking into account instructional skills, class management, personal qualities, and student-faculty relations. Please rate your teacher on a scale of 1 to 5 with 5 as excellent. </p>
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-12" style="text-align:center;">
-                    <li>
-                      <i style="font-size: 13px;"> Excellent </i>
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Excellent">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Very Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Good">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Fair">
-                    </li>
-                    <li>
-                      <input type="radio" name="instSkill1" value="Poor">
-                    </li>
-                    <li>
-                      <i style="font-size: 13px;"> Poor </i>
-                    </li>
-                </div>
-            </div>
-            <br>
-            <div class="row" style="background-color:#7b1113;">
-              <p style="font-size: 19px; color: #e9dbc1; padding-left:10px; margin-top:5px; margin-bottom: 3px;">Additional Comments</p>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> My teacher's strong points are: </p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12" style="margin-top: 3px;">
-                  <textarea class="form-control" style="font-size: 13px;" rows="3" placeholder="Sample Text" readonly></textarea>
-              </div>
-            </div>
+                <?php foreach($value as $q):?>
+                    <div class="row" style="margin-top: 3px;">
+                      <div class="col-12" style="margin-top: 3px;">
+                        <p style="font-size: 15px; color: #7b1113; padding-top: 3px;"> <?=$q->question_text;?> </p>
+                      </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12" style="text-align:center;">
+                            <?php if ($key === 'Comments'):?>
+                                <div class="inputBox">
+                                    <?php $name = 'review_answer_' . $q->id;?>
+                                    <div class="row">
+                                        <div class="col-12" style="margin-top: 3px;">
+                                            <textarea class="form-control" style="font-size: 13px;" name="<?=$name?>" rows="3" readonly><?=set_value($name, $prevAnswers ? $prevAnswers[$index]['answer_text']: '')?></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php else:?>
+                                <li>
+                                    <i style="font-size: 13px;"> Excellent </i>
+                                </li>
+                                <?php $name = 'review_choices_' . $q->id;?>
+                                <?php foreach($choices[$key] as $choice):?>
+                                    <li>
+                                        <?php if(count($prevAnswers) != 0 && $prevAnswers[$index]['qChoice_id'] === $choice['id']):?>
+                                            <input type="radio" name="<?=$name;?>" value="<?=$choice['id'];?>" <?=set_radio($name, $choice['id'], TRUE)?> disabled>
+                                        <?php else:?>
+                                            <input type="radio" name="<?=$name;?>" value="<?=$choice['id'];?>" <?=set_radio($name, $choice['id'])?> disabled>
+                                        <?php endif;?>
+                                    </li>
+                                <?php endforeach;?>
+                                <li>
+                                    <i style="font-size: 13px;"> Poor </i>
+                                </li>
+                            <?php endif;?>
+                        </div>
+                    </div>
+                    <?php $index++;?>
+                <?php endforeach;?>
+                <br>
+            <?php endforeach;?>
             <br>
             <div class="row">
               <div class="col-4">
                 <div style="text-align: center;">
-                  <input type="button" class="button"  style="border-radius: 2rem !important; margin-top: 20px; width: 100px; height: 40px; margin-right: 20px;" data-dismiss="modal" value="Submit">
-                  <input type="button" class="button"  style="border-radius: 2rem !important; margin-top: 20px; width: 100px; height: 40px;" data-dismiss="modal" value="Close">
+                  <input type="button" class="button"  style="border-radius: 2rem !important; margin-top: 20px; width: 100%" data-dismiss="modal" value="Submit">
+                </div>
+              </div>
+              <div class="col-4">
+                <div style="text-align: center;">
+                  <br>
                 </div>
               </div>  
+              <div class="col-4">
+                <div style="text-align: center;">
+                  <input type="button" class="button"  style="border-radius: 2rem !important; margin-top: 20px; width: 100%" data-dismiss="modal" value="Close">
+                </div>
+              </div>
             </div>
           </div>
         </div>

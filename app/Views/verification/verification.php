54ac3b547880144efdb0ae0cc6aed710b846f463
Patrick Castro <jgcastro6@up.edu.ph>
2021-06-01 00:10:08 +0800

    fixed verification

--- app/Views/verification/verification.php
+++ app/Views/verification/verification.php
@@ -6,7 +6,7 @@
             <h1 style="margin-top: 5.2rem; margin-bottom: -3%;"> An email has been sent to verify this activity. <h1>
             <h2 style="color:grey"> You have 30 minutes before the verification email will expire. <h2>
             <br><br>
-            <a href="<?=base_url();?>/dashboard/logout"><button class="button" style="height:50px; font-size: 15px;">Cancel Verification</button></a> 
+            <a href="<?=base_url();?>/dashboard/logout"><button class="button" style="height:50px; font-size: 15px; border-radius:2rem;">Cancel Verification</button></a> 
         </div>    
     </div> 
 <?= $this->endSection();?>

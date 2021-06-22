d0dda6a80438e803ba34dca9fa15e1b796e4e114
rosalierazonable <rsrazonable1@up.edu.ph>
2021-06-22 18:52:39 +0800

    add get latest evaluation, archive eval_sheet implementation

--- app/Models/EvaluationModel.php
+++ app/Models/EvaluationModel.php
@@ -26,4 +26,41 @@ class EvaluationModel extends Model {
   protected $updatedField  = 'updated_on';
 
   protected $skipValidation = true;
+
+  /*
+  * Fetch the latest evaluation
+  */
+  public function get_latest_evaluation() {
+    $db = \Config\Database::connect();
+
+    $sql = <<<EOT
+SELECT id
+FROM evaluation
+WHERE
+	is_deleted = 0
+ORDER BY id DESC
+LIMIT 1
+EOT;
+
+    $query = $db->query($sql);
+    return $query->getResult();
+  }
+
+
+  /*
+  * Archive Evaluation Sheet
+  */
+  public function archive_eval_sheet($eval_id) {
+    $db = \Config\Database::connect();
+
+    $sql = <<<EOT
+UPDATE eval_sheet
+SET is_deleted = 1
+WHERE
+	is_deleted = 0 AND evaluation_id = $eval_id
+EOT;
+
+    $query = $db->query($sql);
+    return $query->getResult();
+  }
 }

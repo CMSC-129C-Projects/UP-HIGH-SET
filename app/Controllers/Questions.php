44199cd33d57b87d890f67e63faad755c8b6f3cf
crispaulrubi <crispauljohn.rubi@gmail.com>
2021-06-24 00:09:58 +0800

    feature-updatequestions

--- app/Controllers/Questions.php
+++ app/Controllers/Questions.php
@@ -9,6 +9,7 @@ use App\Models\EvalSheetModel;
 use App\Models\UserModel;
 use App\Models\FacultyModel;
 use App\Models\SectionModel;
+use App\Models\EvalquestionModel;
 
 
 class Questions extends BaseController
@@ -21,15 +22,34 @@ class Questions extends BaseController
     public function index()
     {
         // Do something
+        $data = [
+            'questions' => $this->get_questions()
+        ];
+
+        return view('linkofquestionsview', $data);
     }
 
     protected function get_questions()
     {
         $sectionModel = new SectionModel();
+        $questionModel = new EvalquestionModel();
 
         $questions = [];
-        $categoryIDs = $sectionModel->get_ids();
+        // $categoryIDs = $sectionModel->get_ids();
+        $questions = $questionModel->where('is_deleted', 0)->findAll();
 
-        return $questions;
+
+        return $this->segregate_questions($questions);
+    }
+
+    protected function segregate_questions($questions)
+    {
+        $separated_questions = [];
+
+        foreach($questions as $question) {
+            $separated_questions[$question['section_id']] = $question['question_text'];
+        }
+
+        return $separated_questions;
     }
 }

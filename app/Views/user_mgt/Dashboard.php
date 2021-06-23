f43253fa4c67e33c80b8542a2f8d330492e3bd4b
Patrick Castro <jgcastro6@up.edu.ph>
2021-06-24 06:18:50 +0800

    change css

--- app/Views/user_mgt/Dashboard.php
+++ app/Views/user_mgt/Dashboard.php
@@ -9,18 +9,6 @@
                     <img src="public/Welcome Message.png" class="img-fluid" alt="Responsive image">
                 </div>
             </div>
-            <!-- <div id="statistics">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card1">
-                            <div class="card-body" style="padding:2rem; text-align:center;">
-                                <p>EVALUATION STATUS OVERVIEW</p>
-                                <p>START DATE ---- END DATE</p>
-                                <p>Student Finished / Student Overall
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
         </section>
     </section>
 <?= $this->endSection();?>
